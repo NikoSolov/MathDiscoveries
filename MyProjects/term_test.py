ca6b1b1d2af586da9537de373825da10a7c16935
NikoSolov <claussolov7939@gmail.com>
2025-05-16 10:01:40 +0300

    Convert from pygame to matplotlib

---
+++ MyProjects/term_test.py
@@ -0,0 +1,12 @@
+def link(uri, label=None):
+    
+    if label is None:
+        label = uri
+    parameters = ''
+
+    # OSC 8 ; params ; URI ST <name> OSC 8 ;; ST
+    escape_mask = '\033]8;{};{}\033\\{}\033]8;;\033\\'
+
+    return escape_mask.format(parameters, uri, label)
+
+print(link('https://github.com/', 'example'))
